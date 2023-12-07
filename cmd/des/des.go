ba2318c48ad5fbe23c59fafd6495adedc0156454
EgorKo25 <egorko25@gmail.com>
2023-12-07 22:25:15 +0300

    init

--- cmd/des/des.go
+++ cmd/des/des.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/EgorKo25/DES/internal/server/service"
+	"go.uber.org/zap"
+
+	"github.com/EgorKo25/DES/internal/config"
 
 	"github.com/EgorKo25/DES/internal/logger"
 	"github.com/EgorKo25/DES/internal/workers"
 )
 
 func main() {
-
 	ctx := context.Background()
 
-	channel := make(chan chan []byte)
+	logs := logger.NewLogger()
+	log := logs["logger"]
+	log.Info("logger was init successful")
 
-	log := logger.NewLoggers()
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal("cannot load config",
+			zap.NamedError("error", err),
+		)
+	}
 
-	worker := workers.NewWorkerPull(ctx, channel,
-		6, 8, 3,
-		log[1].Sugar(), "", "")
+	channel := make(chan chan []byte, cfg.ChannelSize)
 
-	log[0].Info("SERVICE INIT SUCCESS")
+	_ = workers.NewWorkerPull(ctx, channel,
+		cfg.WorkerConfig.MaxWorkers,
+		cfg.WorkerConfig.TimeoutConnection,
+		cfg.WorkerConfig.MaxTimeForResponse,
+		cfg.WorkerConfig.Authentication.Login,
+		cfg.WorkerConfig.Authentication.Password,
+		log,
+		logs["http"],
+	)
 
+	log.Info("worker pull init successful")
+
+	server := service.NewExtServer(
+		channel,
+		log,
+		logs["grpc"],
+	)
+
+	s, err := server.StartServer(cfg.ServiceConfig.IP, cfg.ServiceConfig.PORT)
+	if err != nil {
+		log.Fatal("cannot start grpc server",
+			zap.NamedError("error", err),
+		)
+	}
+	log.Info("server started",
+		zap.String("host", cfg.ServiceConfig.IP),
+		zap.String("port", cfg.ServiceConfig.PORT),
+	)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-interrupt
+	log.Info("Received signal", zap.String("signal", sig.String()))
+	s.GracefulStop()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,26 @@
 module github.com/EgorKo25/DES
 
 go 1.21.1
+
+replace google.golang.org/grpc => github.com/grpc/grpc-go v1.59.0
+
+require (
+	github.com/enescakir/emoji v1.0.0
+	github.com/stretchr/testify v1.8.1
+	github.com/valyala/fastjson v1.6.4
+	go.uber.org/zap v1.26.0
+	google.golang.org/grpc v0.0.0-00010101000000-000000000000
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
