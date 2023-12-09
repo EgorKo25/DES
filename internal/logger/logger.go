55b62ce350eebe3d7dcf32c8e656afa68d82c4ea
EgorKo25 <egorko25@gmail.com>
2023-12-09 03:41:33 +0300

    исправлены ошибки из предупреждений линтера

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,13 +2,21 @@ package logger
 
 import (
 	"os"
+	"path/filepath"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func getWriteSyncer(filename string) zapcore.WriteSyncer {
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	err := os.MkdirAll("logs", 0750)
+	if err != nil {
+		panic(err)
+	}
+
+	filename = filepath.Join("logs/", filepath.Clean(filename))
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		panic(err)
 	}
@@ -33,12 +41,12 @@ func NewLogger() map[string]*zap.Logger {
 		zapcore.NewCore(consoleEncoder, console, warErrLvl),
 		zapcore.NewCore(
 			fileEncoder,
-			zapcore.AddSync(getWriteSyncer("logs/debug.log")),
+			zapcore.AddSync(getWriteSyncer("debug.log")),
 			zap.NewAtomicLevelAt(zap.DebugLevel),
 		),
 		zapcore.NewCore(
 			fileEncoder,
-			zapcore.AddSync(getWriteSyncer("logs/warning_error.log")),
+			zapcore.AddSync(getWriteSyncer("warning_error.log")),
 			warErrLvl,
 		),
 	)
@@ -49,7 +57,7 @@ func NewLogger() map[string]*zap.Logger {
 		zapcore.NewCore(consoleEncoder, console, zap.InfoLevel),
 		zapcore.NewCore(
 			fileEncoder,
-			zapcore.AddSync(getWriteSyncer("logs/http.log")),
+			zapcore.AddSync(getWriteSyncer("http.log")),
 			zap.NewAtomicLevelAt(zap.DebugLevel),
 		),
 	)
@@ -60,7 +68,7 @@ func NewLogger() map[string]*zap.Logger {
 		zapcore.NewCore(consoleEncoder, console, zap.InfoLevel),
 		zapcore.NewCore(
 			fileEncoder,
-			zapcore.AddSync(getWriteSyncer("logs/grpc.log")),
+			zapcore.AddSync(getWriteSyncer("grpc.log")),
 			zap.NewAtomicLevelAt(zap.DebugLevel),
 		),
 	)
